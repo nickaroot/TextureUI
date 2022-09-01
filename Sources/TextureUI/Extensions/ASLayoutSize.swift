472341428bc855ecba8d46c62b6ed1c68365dd84
Nick <nickaroot@icloud.com>
2022-09-01 23:33:50 +0300

    ASLayoutSize  extension

---
+++ Sources/TextureUI/Extensions/ASLayoutSize.swift
@@ -0,0 +1,14 @@
+//
+//  ASDimension.swift
+//  
+//
+//  Created by Nikita Arutyunov on 07.03.2022.
+//
+
+import AsyncDisplayKit
+
+extension ASLayoutSize {
+    public static var auto: Self {
+        ASLayoutSizeAuto
+    }
+}
