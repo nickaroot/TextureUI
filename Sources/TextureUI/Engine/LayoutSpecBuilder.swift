a106257adc2a5d50618bfbe281278177ee047a9e
Nikita Arutiunov <funtrum@icloud.com>
2022-09-20 20:02:47 +0300

    LayoutSpecBuilder refactoring, Tests

--- Sources/TextureUI/Engine/LayoutSpecBuilder.swift
+++ Sources/TextureUI/Engine/LayoutSpecBuilder.swift
@@ -9,101 +9,235 @@ import AsyncDisplayKit
 
 @resultBuilder
 public struct LayoutSpecBuilder {
-
-    // MARK: - Empty
-
-    /// Builds an empty view from a block containing no statements.
-    public static func buildBlock() -> Layout { .empty }
-
-    // MARK: - Single
-
-    /// Passes a single node written as a child node through unmodified.
-    ///
-    /// - Parameter content: Layout Content
-    ///
-    /// - Returns: Layout
-    ///
-    /// An example of a single node written as a child node is
-    /// `{ ASDisplayNode() }`.
-    public static func buildBlock<Content>(_ content: Content) -> Layout
-    where Content: LayoutElement {
+    public static func buildBlock() -> Layout {
+        .empty
+    }
+    
+    public static func buildBlock(_ layout: Layout...) -> Layout {
+        .composite(layout)
+    }
+    
+    public static func buildBlock(_ layout: Layout?...) -> Layout {
+        let layouts = layout.compactMap { $0 }
+        
+        guard
+            !layouts.isEmpty
+        else {
+            return .empty
+        }
+        
+        if layouts.count == 1, let layout = layouts.first {
+            return layout
+        }
+        
+        return .composite(layouts)
+    }
+    
+    public static func buildBlock<Content>(
+        _ content: Content...
+    ) -> Layout where Content: LayoutElement {
+        .composite(content)
+    }
+    
+    public static func buildBlock<Content>(
+        _ content: Content?...
+    ) -> Layout where Content: LayoutElement {
+        let content = content.compactMap { $0 }
+        
+        guard
+            !content.isEmpty
+        else {
+            return .empty
+        }
+        
+        if content.count == 1, let content = content.first {
+            return .single(content)
+        }
+        
+        return .composite(content)
+    }
+    
+    public static func buildOptional(_ layout: Layout?) -> Layout {
+        guard
+            let layout = layout
+        else {
+            return .empty
+        }
+        
+        return layout
+    }
+    
+    public static func buildOptional(_ content: (some LayoutElement)?) -> Layout {
+        guard
+            let content = content
+        else {
+            return .empty
+        }
+        
+        return .single(content)
+    }
+    
+    public static func buildEither(first layout: Layout) -> Layout {
+        layout
+    }
+    
+    public static func buildEither(first layout: Layout?) -> Layout {
+        guard
+            let layout = layout
+        else {
+            return .empty
+        }
+        
+        return layout
+    }
+    
+    public static func buildEither(second layout: Layout) -> Layout {
+        layout
+    }
+    
+    public static func buildEither(second layout: Layout?) -> Layout {
+        guard
+            let layout = layout
+        else {
+            return .empty
+        }
+        
+        return layout
+    }
+    
+    public static func buildEither(first content: some LayoutElement) -> Layout {
         .single(content)
     }
-
-    public static func buildOptional<Content>(_ content: Content?) -> Layout
-    where Content: LayoutElement {
-        content.map { .single($0) } ?? .empty
+    
+    public static func buildEither(first content: (some LayoutElement)?) -> Layout {
+        guard
+            let content = content
+        else {
+            return .empty
+        }
+        
+        return .single(content)
     }
-
-    /// Provides support for “if” statements in multi-statement closures,
-    /// producing an optional view that is visible only when the condition
-    /// evaluates to `true`.
-//    public static func buildIf<Content>(_ content: Content?) -> Layout?
-//    where Content: LayoutElement {
-//        content.map { .single($0) }
-//    }
-    public static func buildIf<Content>(_ content: Content?) -> Content? where Content: LayoutElement {
-        content
-    }
-
-    /// Provides support for "if" statements in multi-statement closures,
-    /// producing conditional content for the "then" branch.
-    public static func buildEither<TrueContent, FalseContent>(
-        first: TrueContent
-    ) -> ConditionalContent<TrueContent, FalseContent>
-    where TrueContent: LayoutElement, FalseContent: LayoutElement {
-        ConditionalContent<TrueContent, FalseContent>(.trueContent(first))
-    }
-
-    public static func buildEither(first component: Layout) -> Layout {
-        component
-    }
-
-    /// Provides support for "if-else" statements in multi-statement closures,
-    /// producing conditional content for the "else" branch.
-    public static func buildEither<TrueContent, FalseContent>(
-        second: FalseContent
-    ) -> ConditionalContent<TrueContent, FalseContent>
-    where TrueContent: LayoutElement, FalseContent: LayoutElement {
-        ConditionalContent<TrueContent, FalseContent>(.falseContent(second))
-    }
-
-    public static func buildEither(second component: Layout) -> Layout {
-        component
-    }
-
-    public static func buildExpression<Content>(_ expression: Content) -> Layout
-    where Content: LayoutElement {
-        .single(expression)
-    }
-
-    public static func buildExpression<Content>(_ expression: Content?) -> Layout
-    where Content: LayoutElement {
-        expression.map { .single($0) } ?? .empty
-    }
-
-    // MARK: - Composite
-
-    @_disfavoredOverload
-    public static func buildBlock<Content>(_ contents: Content...) -> Layout
-    where Content: LayoutElement {
-        .composite(contents)
-    }
-
-    @_disfavoredOverload
-    public static func buildBlock<Content>(_ contents: Content?...) -> Layout
-    where Content: LayoutElement {
-        .composite(contents.compactMap { $0 })
-    }
-}
-
-@available(iOS 14.0, macOS 11.0, tvOS 14.0, watchOS 7.0, *)
-extension LayoutSpecBuilder {
-    /// Provides support for "if" statements with `#available()` clauses in
-    /// multi-statement closures, producing conditional content for the "then"
-    /// branch, i.e. the conditionally-available branch.
-    public static func buildLimitedAvailability<Content>(_ content: Content) -> Layout
-    where Content: LayoutElement {
+    
+    public static func buildEither(second content: some LayoutElement) -> Layout {
         .single(content)
+    }
+    
+    public static func buildEither(second content: (some LayoutElement)?) -> Layout {
+        guard
+            let content = content
+        else {
+            return .empty
+        }
+        
+        return .single(content)
+    }
+    
+    public static func buildArray(_ layout: [Layout]) -> Layout {
+        if layout.count == 1, let layout = layout.first {
+            return layout
+        }
+        
+        return .composite(layout)
+    }
+    
+    public static func buildArray(_ layout: [Layout?]) -> Layout {
+        let layout = layout.compactMap { $0 }
+        
+        guard
+            !layout.isEmpty
+        else {
+            return .empty
+        }
+        
+        if layout.count == 1, let layout = layout.first {
+            return layout
+        }
+        
+        return .composite(layout)
+    }
+    
+    public static func buildArray(_ content: [some LayoutElement]) -> Layout {
+        if content.count == 1, let content = content.first {
+            return .single(content)
+        }
+        
+        return .composite(content)
+    }
+    
+    public static func buildArray(_ content: [(some LayoutElement)?]) -> Layout {
+        let content = content.compactMap { $0 }
+        
+        guard
+            !content.isEmpty
+        else {
+            return .empty
+        }
+        
+        if content.count == 1, let content = content.first {
+            return .single(content)
+        }
+        
+        return .composite(content)
+    }
+    
+    public static func buildExpression(_ layout: Layout) -> Layout {
+        layout
+    }
+    
+    public static func buildExpression(_ layout: Layout?) -> Layout {
+        guard
+            let layout = layout
+        else {
+            return .empty
+        }
+        
+        return layout
+    }
+    
+    public static func buildExpression(_ content: some LayoutElement) -> Layout {
+        .single(content)
+    }
+    
+    public static func buildExpression(_ content: (some LayoutElement)?) -> Layout {
+        guard
+            let content = content
+        else {
+            return .empty
+        }
+        
+        return .single(content)
+    }
+    
+    @available(iOS 14.0, macOS 11.0, tvOS 14.0, watchOS 7.0, *)
+    public static func buildLimitedAvailability(_ layout: Layout) -> Layout {
+        layout
+    }
+    
+    @available(iOS 14.0, macOS 11.0, tvOS 14.0, watchOS 7.0, *)
+    public static func buildLimitedAvailability(_ layout: Layout?) -> Layout {
+        guard
+            let layout = layout
+        else {
+            return .empty
+        }
+        
+        return layout
+    }
+    
+    @available(iOS 14.0, macOS 11.0, tvOS 14.0, watchOS 7.0, *)
+    public static func buildLimitedAvailability(_ content: some LayoutElement) -> Layout {
+        .single(content)
+    }
+    
+    @available(iOS 14.0, macOS 11.0, tvOS 14.0, watchOS 7.0, *)
+    public static func buildLimitedAvailability(_ content: (some LayoutElement)?) -> Layout {
+        guard
+            let content = content
+        else {
+            return .empty
+        }
+        
+        return .single(content)
     }
 }
--- Tests/TextureUITests/TextureUITests.swift
+++ Tests/TextureUITests/TextureUITests.swift
@@ -1,4 +1,194 @@
 import XCTest
 @testable import TextureUI
 
-final class TextureUITests: XCTestCase { }
+final class TextureUITests: XCTestCase {
+    func buildEmptyBlockTest() -> ASLayoutSpec {
+        LayoutSpec { }
+    }
+    
+    func buildBlockWithLayoutTest() -> ASLayoutSpec {
+        LayoutSpec {
+            Layout.single(ASDisplayNode())
+        }
+    }
+    
+    func buildBlockWithOptionalLayoutTest() -> ASLayoutSpec {
+        LayoutSpec {
+            Layout?.none
+        }
+    }
+    
+    func buildBlockWithLayoutElementTest() -> ASLayoutSpec {
+        LayoutSpec {
+            ASDisplayNode()
+        }
+    }
+    
+    func buildBlockWithOptionalLayoutElementTest() -> ASLayoutSpec {
+        LayoutSpec {
+            ASDisplayNode?.none
+        }
+    }
+    
+    func buildIfWithLayoutTest() -> ASLayoutSpec {
+        LayoutSpec {
+            if Int.random(in: 0..<1) == 0 {
+                Layout.single(ASDisplayNode())
+            }
+        }
+    }
+    
+    func buildIfWithOptionalLayoutTest() -> ASLayoutSpec {
+        LayoutSpec {
+            if Int.random(in: 0..<1) == 0 {
+                Layout?.none
+            }
+        }
+    }
+    
+    func buildIfWithLayoutElementTest() -> ASLayoutSpec {
+        LayoutSpec {
+            if Int.random(in: 0..<1) == 0 {
+                ASDisplayNode()
+            }
+        }
+    }
+    
+    func buildIfWithOptionalLayoutElementTest() -> ASLayoutSpec {
+        LayoutSpec {
+            if Int.random(in: 0..<1) == 0 {
+                ASDisplayNode?.none
+            }
+        }
+    }
+    
+    func buildIfElseWithLayoutTest() -> ASLayoutSpec {
+        LayoutSpec {
+            if Int.random(in: 0..<1) == 0 {
+                Layout.single(ASDisplayNode())
+            } else {
+                Layout.single(ASDisplayNode())
+            }
+        }
+    }
+    
+    func buildIfElseWithOptionalLayoutTest() -> ASLayoutSpec {
+        LayoutSpec {
+            if Int.random(in: 0..<1) == 0 {
+                Layout?.none
+            } else {
+                Layout?.none
+            }
+        }
+    }
+    
+    func buildIfElseWithLayoutElementTest() -> ASLayoutSpec {
+        LayoutSpec {
+            if Int.random(in: 0..<1) == 0 {
+                ASDisplayNode()
+            } else {
+                ASDisplayNode()
+            }
+        }
+    }
+    
+    func buildIfElseWithOptionalLayoutElementTest() -> ASLayoutSpec {
+        LayoutSpec {
+            if Int.random(in: 0..<1) == 0 {
+                ASDisplayNode?.none
+            } else {
+                ASDisplayNode?.none
+            }
+        }
+    }
+    
+    func buildArrayOfLayoutsTest() -> ASLayoutSpec {
+        LayoutSpec {
+            [0..<100].map { _ in Layout.single(ASDisplayNode()) }
+        }
+    }
+    
+    func buildArrayOfOptionalLayoutsTest() -> ASLayoutSpec {
+        LayoutSpec {
+            [0..<100].map { _ in Layout?.none }
+        }
+    }
+    
+    func buildArrayOfLayoutElementsTest() -> ASLayoutSpec {
+        LayoutSpec {
+            [0..<100].map { _ in ASDisplayNode() }
+        }
+    }
+    
+    func buildArrayOfOptionalsLayoutElementsTest() -> ASLayoutSpec {
+        LayoutSpec {
+            [0..<100].map { _ in ASDisplayNode?.none }
+        }
+    }
+    
+    func buildExpressionWithLayoutTest() -> ASLayoutSpec {
+        LayoutSpec {
+            for layoutElement in [0..<100].map { _ in ASDisplayNode() } {
+                layoutElement
+            }
+        }
+    }
+    
+    func buildExpressionWithOptionalLayoutTest() -> ASLayoutSpec {
+        LayoutSpec {
+            for layoutElement in [0..<100].map { _ in ASDisplayNode?.none } {
+                layoutElement
+            }
+        }
+    }
+    
+    func buildExpressionWithLayoutElementTest() -> ASLayoutSpec {
+        LayoutSpec {
+            for layout in [0..<100].map { _ in Layout.single(ASDisplayNode()) } {
+                layout
+            }
+        }
+    }
+    
+    func buildExpressionWithOptionalLayoutElementTest() -> ASLayoutSpec {
+        LayoutSpec {
+            for layout in [0..<100].map { _ in Layout?.none } {
+                layout
+            }
+        }
+    }
+    
+    
+    
+    func buildLimitedAvailabilityWithLayoutTest() -> ASLayoutSpec {
+        LayoutSpec {
+            if #available(macOS 11.0, iOS 14.0, *) {
+                Layout.single(ASDisplayNode())
+            }
+        }
+    }
+    
+    func buildLimitedAvailabilityWithOptionalLayoutTest() -> ASLayoutSpec {
+        LayoutSpec {
+            if #available(macOS 11.0, iOS 14.0, *) {
+                Layout?.none
+            }
+        }
+    }
+    
+    func buildLimitedAvailabilityWithLayoutElementTest() -> ASLayoutSpec {
+        LayoutSpec {
+            if #available(macOS 11.0, iOS 14.0, *) {
+                ASDisplayNode()
+            }
+        }
+    }
+    
+    func buildLimitedAvailabilityWithOptionalLayoutElementTest() -> ASLayoutSpec {
+        LayoutSpec {
+            if #available(macOS 11.0, iOS 14.0, *) {
+                ASDisplayNode?.none
+            }
+        }
+    }
+}
